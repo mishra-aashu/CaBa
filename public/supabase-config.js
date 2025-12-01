455aae4bceec98c1cb1d128d5a0d6c3789d2f7a6
mishra-aashu <aashutoshk625@gmail.com>
2025-12-01 22:11:03 +0530

    Move Supabase config to separate file and load it in reset-password.html

---
+++ public/supabase-config.js
@@ -0,0 +1,8 @@
+// Supabase Configuration
+const SUPABASE_CONFIG = {
+  url: 'https://riekjnqllkrqkmqxmtfu.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZWtqbnFsbGtycWttcXhtdGZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4ODc3MjQsImV4cCI6MjA3NzQ2MzcyNH0.heQABR_DZFWZ_UIb38Tzdgcy-5z5LSUob0icnqsiiQY'
+};
+
+// Make it available globally
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
